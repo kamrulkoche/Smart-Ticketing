a0e558bc92bb141e185c7ecb02f4d7f77c69ce9f
kamrulkoche <kamrulkoche@gmail.com>
2024-04-02 20:33:01 +0600

    menu add

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,14 @@
-const calcScrollValue = () => {
-    const scrollProgress = document.getElementById("progress");
-    //const progressValue = document.getElementById("progress-value");
-    const pos = document.documentElement.scrollTop;
-    //console.log(pos);
-    const calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    //console.log(calcHeight);
-    const scrollValue = Math.round((pos * 100) / calcHeight);
-    //console.log(scrollValue);
-
-    if (pos > 100) {
-        scrollProgress.style.display = "grid";
-    }
-    else {
-        scrollProgress.style.display = "none";
-    }
-
-    scrollProgress.addEventListener("click", () => {
-        document.documentElement.scrollTop = 0;
-    });
-    scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
-};
-
 window.onscroll = calcScrollValue;
-window.onload = calcScrollValue;
+//window.onload = calcScrollValue;
+
+function openMenu() {
+    showElementById("navMenu");
+    showElementById("closeMenu");
+    hideElementById("openMenu");
+}
+
+function closeMenu() {
+    hideElementById("navMenu");
+    hideElementById("closeMenu");
+    showElementById("openMenu");
+}
---
+++ scripts/utility.js
@@ -0,0 +1,34 @@
+const calcScrollValue = () => {
+    const scrollProgress = document.getElementById("progress");
+    //const progressValue = document.getElementById("progress-value");
+    const pos = document.documentElement.scrollTop;
+    //console.log(pos);
+    const calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    //console.log(calcHeight);
+    const scrollValue = Math.round((pos * 100) / calcHeight);
+    //console.log(scrollValue);
+
+    if (pos > 100) {
+        scrollProgress.style.display = "grid";
+    }
+    else {
+        scrollProgress.style.display = "none";
+    }
+
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add("hidden");
+}
+
+function showElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove("hidden");
+}
+
+
