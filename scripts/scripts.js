813f1d185399a8861f9ae75407ece59e53b2bf10
kamrulkoche <kamrulkoche@gmail.com>
2024-04-03 15:11:34 +0600

    jquery add

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,13 +2,27 @@ window.onscroll = calcScrollValue;
 //window.onload = calcScrollValue;
 
 function openMenu() {
-    showElementById("navMenu");
-    showElementById("closeMenu");
-    hideElementById("openMenu");
+  showElementById("navMenu");
+  showElementById("closeMenu");
+  hideElementById("openMenu");
 }
 
 function closeMenu() {
-    hideElementById("navMenu");
-    hideElementById("closeMenu");
-    showElementById("openMenu");
+  hideElementById("navMenu");
+  hideElementById("closeMenu");
+  showElementById("openMenu");
 }
+
+
+function A1() {
+  console.log("sdsds");
+  removeBackgroundColorById("A1");
+  setBackgroundColorById("A1");
+}
+
+
+$(document).ready(function(){
+    $("p").click(function(){
+      $(this).hide();
+    });
+  });
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,34 +1,44 @@
 const calcScrollValue = () => {
-    const scrollProgress = document.getElementById("progress");
-    //const progressValue = document.getElementById("progress-value");
-    const pos = document.documentElement.scrollTop;
-    //console.log(pos);
-    const calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    //console.log(calcHeight);
-    const scrollValue = Math.round((pos * 100) / calcHeight);
-    //console.log(scrollValue);
+  const scrollProgress = document.getElementById("progress");
+  //const progressValue = document.getElementById("progress-value");
+  const pos = document.documentElement.scrollTop;
+  //console.log(pos);
+  const calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  //console.log(calcHeight);
+  const scrollValue = Math.round((pos * 100) / calcHeight);
+  //console.log(scrollValue);
 
-    if (pos > 100) {
-        scrollProgress.style.display = "grid";
-    }
-    else {
-        scrollProgress.style.display = "none";
-    }
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
 
-    scrollProgress.addEventListener("click", () => {
-        document.documentElement.scrollTop = 0;
-    });
-    scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
 };
 
 function hideElementById(elementId) {
-    const element = document.getElementById(elementId);
-    element.classList.add("hidden");
+  const element = document.getElementById(elementId);
+  element.classList.add("hidden");
 }
 
 function showElementById(elementId) {
-    const element = document.getElementById(elementId);
-    element.classList.remove("hidden");
+  const element = document.getElementById(elementId);
+  element.classList.remove("hidden");
 }
 
+function removeBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("bg-[#bfbfbf]");
+  element.classList.remove("hover:bg-slate-400");
+}
 
+function setBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("bg-[#1DD100]");
+}
